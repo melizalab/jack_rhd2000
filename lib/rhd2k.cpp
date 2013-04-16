6f60a389c60698b278a1ad5faafd4fff6256dc95
Dan Meliza <dan@meliza.org>
2013-04-16 17:56:26 -0500

    work on encapsulation of individual amplifiers

--- lib/rhd2k.cpp
+++ lib/rhd2k.cpp
@@ -1,48 +1,204 @@
+#include <boost/utility/binary.hpp>
+#include <cmath>
+#include <bitset>
 #include "rhd2k.hpp"
+
+static unsigned char[] default_registers =
+{ BOOST_BINARY(11011110),      // 0: mostly fixed values; fast settle [5] off
+  BOOST_BINARY(01000000),      // 1: set ADC buffer bias [5:0] based on sample rate
+  BOOST_BINARY(00000000),      // 2: set mux bias [5:0] based on sample rate
+  BOOST_BINARY(00000010),      // 3: digout HiZ [1] = 1
+  BOOST_BINARY(10000000),      // 4: set DSP enable [4] and cutoff [3:0] in class
+  BOOST_BINARY(00000000),      // 5: impedance check DAC disabled
+  BOOST_BINARY(10000000),      // 6: DAC set to 0
+  BOOST_BINARY(00000000),      // 7: zcheck DAC disconnected
+  BOOST_BINARY(00000000),      // 8: offchip rh1 [7] disabled; set rh1 value [5:0] in class
+  BOOST_BINARY(10000000),      // 9: adc1 enabled [7]; set rh1 value [4:0] in class
+  BOOST_BINARY(00000000),      // 10: offchip rh2 [7] disabled; set rh2 value [5:0] in class
+  BOOST_BINARY(10000000),      // 11: adc2 enabled [7]; set rh2 value [4:0] in class
+  BOOST_BINARY(00000000),      // 12: offchip rl [7] disabled; set rl dac1 [6:0] in class
+  BOOST_BINARY(10000000),      // 13: adc3 enabled [7]; set rl dac3 [6] and dac2 [5:0] in class
+  BOOST_BINARY(11111111),      // 14: amps 0-7 enabled
+  BOOST_BINARY(11111111),      // 15: amps 8-15 enabled
+  BOOST_BINARY(11111111),      // 16: amps 16-23 enabled
+  BOOST_BINARY(11111111)       // 17: amps 23-31 enabled
+};
+
+static const double RH1Base = 2200.0;
+static const double RH1Dac1Unit = 600.0;
+static const double RH1Dac2Unit = 29400.0;
+static const int RH1Dac1Steps = 63;
+static const int RH1Dac2Steps = 31;
+
+static const double RH2Base = 8700.0;
+static const double RH2Dac1Unit = 763.0;
+static const double RH2Dac2Unit = 38400.0;
+static const int RH2Dac1Steps = 63;
+static const int RH2Dac2Steps = 31;
+
+// Returns the value of the RH1 resistor (in ohms) corresponding to a particular upper
+// bandwidth value (in Hz).
+static double
+rH1FromUpperBandwidth(double upperBandwidth)
+{
+    double log10f = log10(upperBandwidth);
+
+    return 0.9730 * pow(10.0, (8.0968 - 1.1892 * log10f + 0.04767 * log10f * log10f));
+}
+
+// Returns the value of the RH2 resistor (in ohms) corresponding to a particular upper
+// bandwidth value (in Hz).
+static double
+rH2FromUpperBandwidth(double upperBandwidth)
+{
+    double log10f = log10(upperBandwidth);
+
+    return 1.0191 * pow(10.0, (8.1009 - 1.0821 * log10f + 0.03383 * log10f * log10f));
+}
+
+// Returns the value of the RL resistor (in ohms) corresponding to a particular lower
+// bandwidth value (in Hz).
+static double
+rLFromLowerBandwidth(double lowerBandwidth)
+{
+    double log10f = log10(lowerBandwidth);
+
+    if (lowerBandwidth < 4.0) {
+        return 1.0061 * pow(10.0, (4.9391 - 1.2088 * log10f + 0.5698 * log10f * log10f +
+                                   0.1442 * log10f * log10f * log10f));
+    } else {
+        return 1.0061 * pow(10.0, (4.7351 - 0.5916 * log10f + 0.08482 * log10f * log10f));
+    }
+}
+
+// Returns the amplifier upper bandwidth (in Hz) corresponding to a particular value
+// of the resistor RH1 (in ohms).
+static double
+upperBandwidthFromRH1(double rH1)
+{
+    double a, b, c;
+
+    a = 0.04767;
+    b = -1.1892;
+    c = 8.0968 - log10(rH1/0.9730);
+
+    return pow(10.0, ((-b - sqrt(b * b - 4 * a * c))/(2 * a)));
+}
+
+// Returns the amplifier upper bandwidth (in Hz) corresponding to a particular value
+// of the resistor RH2 (in ohms).
+static double
+upperBandwidthFromRH2(double rH2)
+{
+    double a, b, c;
+
+    a = 0.03383;
+    b = -1.0821;
+    c = 8.1009 - log10(rH2/1.0191);
+
+    return pow(10.0, ((-b - sqrt(b * b - 4 * a * c))/(2 * a)));
+}
+
+// Returns the amplifier lower bandwidth (in Hz) corresponding to a particular value
+// of the resistor RL (in ohms).
+static double
+lowerBandwidthFromRL(double rL)
+{
+    double a, b, c;
+
+    // Quadratic fit below is invalid for values of RL less than 5.1 kOhm
+    if (rL < 5100.0) {
+        rL = 5100.0;
+    }
+
+    if (rL < 30000.0) {
+        a = 0.08482;
+        b = -0.5916;
+        c = 4.7351 - log10(rL/1.0061);
+    } else {
+        a = 0.3303;
+        b = -1.2100;
+        c = 4.9873 - log10(rL/1.0061);
+    }
+
+    return pow(10.0, ((-b - sqrt(b * b - 4 * a * c))/(2 * a)));
+}
+
 
 using namespace rhd2k;
 
 rhd2000::rhd2000(unsigned int sampling_rate)
         : _sampling_rate(sampling_rate),
-          ampFastSettle(1),
-          tempS1(0),
-          tempS2(0),
-          tempEn(0),
-
+          _registers({0})
 {
-        dsp_enabled() = true;
+        memcpy(_registers, default_registers, sizeof(default_registers));
         set_dsp_cutoff(1.0);
+        set_upper_bandwidth(10000);
+        set_lower_bandwidth(1.0);
 }
 
-double const &
-rhd2000::upper_bandwidth() const;
-
 double
-rhd2000::set_upper_bandwidth(double);
+rhd2000::upper_bandwidth() const {
+        int rh1dac1 = _registers[8] & 0x1f;
+        int rh1dac2 = _registers[9] & 0x0f;
+        int rh2dac1 = _registers[10] & 0x1f;
+        int rh2dac2 = _registers[11] & 0x0f;
 
-double const &
-rhd2000::lower_bandwidth() const;
-
-double
-rhd2000::set_lower_bandwidth(double);
-
-bool &
-rhd2000::dsp_enabled();
-
-bool const &
-rhd2000::dsp_enabled() const;
-
-double const &
-rhd2000::dsp_cutoff() const;
-
-double
-rhd2000::set_dsp_cutoff(double);
+        double rh1 = RH1Base + RH1Dac1Unit * rh1dac1 + RH1Dac2Unit * rh1dac2;
+        double rh2 = RH2Base + RH2Dac1Unit * rh2dac1 + RH2Dac2Unit * rh2dac2;
+        return sqrt(upperBandwidthFromRH1(rh1) * upperBandwidthFromRH2(rh2));
+}
 
 void
-rhd2000::set_amp_power(short channel, bool powered);
+rhd2000::set_upper_bandwidth(double)
+{}
 
-std::vector<int>
-rhd2000::amp_power() const;
+double
+rhd2000::lower_bandwidth() const
+{
+        int rldac1 = _registers[12] & 0x3f;
+        int rldac2 = _registers[13] & 0x1f;
+        int rldac3 = _registers[13] & 0x20;
+        double rl = RLBase + RLDac1Unit * rldac1 + RLDac2Unit * rldac2 + RLDac3Unit * rldac3;
+        return lowerBandwidthFromRL(rl);
+}
+
+void
+rhd2000::set_lower_bandwidth(double)
+{}
+
+bool
+rhd2000::dsp_enabled() const
+{
+        return (_registers[4] & 0x08) == 1;
+}
+
+double
+rhd2000::dsp_cutoff() const
+{
+        const double Pi = 2*acos(0.0);
+        double x = _registers[4] & 0x07;
+        x = pow(2.0, x);
+        return _sampling_rate * log(x / (x - 1.0)) / (2*Pi);
+}
+
+void
+rhd2000::set_dsp_cutoff(double)
+{}
+
+void
+rhd2000::set_amp_power(unsigned short channel, bool powered)
+{
+        assert(channel <= 32);
+        std::size_t reg = (channel / 8) + 14;
+        unsigned char mask = 1 << (channel % 8);
+        if (powered) _registers[reg] |= mask;
+        else _registers[reg] &= ~mask;
+}
+
+
+// std::vector<int>
+// rhd2000::amp_power() const;
 
 void
 rhd2000::update(short const * data);
--- lib/rhd2k.hpp
+++ lib/rhd2k.hpp
@@ -35,19 +35,19 @@ public:
         ~rhd2000() {}
 
         /* public methods to update register values */
-        double const & upper_bandwidth() const;
-        double set_upper_bandwidth(double);
+        double upper_bandwidth() const;
+        void set_upper_bandwidth(double);
 
-        double const & lower_bandwidth() const;
-        double set_lower_bandwidth(double);
+        double lower_bandwidth() const;
+        void set_lower_bandwidth(double);
 
-        bool & dsp_enabled();
-        bool const & dsp_enabled() const;
-        double const & dsp_cutoff() const;
-        double set_dsp_cutoff(double);
+        bool dsp_enabled() const;
+        double dsp_cutoff() const;
+        /** if arg is <= 0, turn off dsp */
+        void set_dsp_cutoff(double);
 
-        void set_amp_power(short channel, bool powered);
-        std::vector<int> amp_power() const;
+        void set_amp_power(unsigned short channel, bool powered);
+        // std::vector<int> amp_power() const;
 
         /** Update internal state with results from regset command. */
         void update(char const * data);
@@ -67,107 +67,108 @@ public:
 
 private:
         unsigned int _sampling_rate;
+        unsigned char _registers[regset_command_length];
 
         // RHD2000 Register 0 variables
         // ADC reference generator bandwidth (0 [highest BW] - 3 [lowest BW]); always set to 3
-        static const int adcReferenceBw = 3;
-        // amplifier fast settle (off = normal operation)
-        int ampFastSettle;
-        // enable amplifier voltage references (0 = power down; 1 = enable); 1 = normal operation
-        static const int ampVrefEnable = 1;
-        // ADC comparator preamp bias current (0 [lowest] - 3 [highest], only
-        // valid for comparator select = 2,3); always set to 3
-        static const int adcComparatorBias = 3;
-        // ADC comparator select; always set to 2
-        static const int adcComparatorSelect = 2;
+        // static const int adcReferenceBw = 3;
+        // // amplifier fast settle (off = normal operation)
+        // int ampFastSettle;
+        // // enable amplifier voltage references (0 = power down; 1 = enable); 1 = normal operation
+        // static const int ampVrefEnable = 1;
+        // // ADC comparator preamp bias current (0 [lowest] - 3 [highest], only
+        // // valid for comparator select = 2,3); always set to 3
+        // static const int adcComparatorBias = 3;
+        // // ADC comparator select; always set to 2
+        // static const int adcComparatorSelect = 2;
 
-        // RHD2000 Register 1 variables
-        // supply voltage sensor enable (0 = disable; 1 = enable)
-        static const int vddSenseEnable = 1;
-        // ADC reference buffer bias current (0 [highest current] - 63 [lowest current]);
-        int adcBufferBias;
+        // // RHD2000 Register 1 variables
+        // // supply voltage sensor enable (0 = disable; 1 = enable)
+        // static const int vddSenseEnable = 1;
+        // // ADC reference buffer bias current (0 [highest current] - 63 [lowest current]);
+        // int adcBufferBias;
 
-        // RHD2000 Register 2 variables
-        // ADC input MUX bias current (0 [highest current] - 63 [lowest current]);
-        int muxBias;
+        // // RHD2000 Register 2 variables
+        // // ADC input MUX bias current (0 [highest current] - 63 [lowest current]);
+        // int muxBias;
 
-        // RHD2000 Register 3 variables
-        // MUX capacitance load at ADC input (0 [min CL] - 7 [max CL]); LSB = 3 pF
-        int muxLoad;
-        // temperature sensor S1 (0-1); 0 = power saving mode
-        int tempS1;
-        // temperature sensor S2 (0-1); 0 = power saving mode
-        int tempS2;
-        // temperature sensor enable (0 = disable; 1 = enable)
-        int tempEn;
-        // auxiliary digital output state
-        static const int digOutHiZ = 1;
-        static const int digOut = 0;
+        // // RHD2000 Register 3 variables
+        // // MUX capacitance load at ADC input (0 [min CL] - 7 [max CL]); LSB = 3 pF
+        // int muxLoad;
+        // // temperature sensor S1 (0-1); 0 = power saving mode
+        // int tempS1;
+        // // temperature sensor S2 (0-1); 0 = power saving mode
+        // int tempS2;
+        // // temperature sensor enable (0 = disable; 1 = enable)
+        // int tempEn;
+        // // auxiliary digital output state
+        // static const int digOutHiZ = 1;
+        // static const int digOut = 0;
 
-        // RHD2000 Register 4 variables
-        // weak MISO (0 = MISO line is HiZ when CS is inactive; 1 = MISO line is
-        // weakly driven when CS is inactive)
-        static const int weakMiso = 1;
-        // two's complement ADC results (0 = unsigned offset representation; 1 =
-        // signed representation)
-        static const int twosComp = 0;
-        // absolute value mode (0 = normal output; 1 = output passed through
-        // abs(x) function)
-        static const int absMode = 0;
-        int dspEn;              // DSP offset removal enable/disable
-        int dspCutoffFreq;      // DSP offset removal HPF cutoff freqeuncy
+        // // RHD2000 Register 4 variables
+        // // weak MISO (0 = MISO line is HiZ when CS is inactive; 1 = MISO line is
+        // // weakly driven when CS is inactive)
+        // static const int weakMiso = 1;
+        // // two's complement ADC results (0 = unsigned offset representation; 1 =
+        // // signed representation)
+        // static const int twosComp = 0;
+        // // absolute value mode (0 = normal output; 1 = output passed through
+        // // abs(x) function)
+        // static const int absMode = 0;
+        // int dspEn;              // DSP offset removal enable/disable
+        // int dspCutoffFreq;      // DSP offset removal HPF cutoff freqeuncy
 
-        // RHD2000 Register 5 variables
-        // impedance testing DAC power-up (0 = power down; 1 = power up)
-        static const int zcheckDacPower = 0;
-        // impedance testing dummy load (0 = normal operation; 1 = insert 60 pF to ground)
-        static const int zcheckLoad = 0;
-        // impedance testing scale factor:
-        // 0x00 = 100 fF
-        // 0x01 = 1.0 pF
-        // 0x03 = 10.0 pF
-        static const int zcheckScale = 0;
-        // impedance testing connect all (0 = normal operation; 1 = connect all electrodes together)
-        static const int zcheckConnAll = 0;
-        // impedance testing polarity select (RHD2216 only) (0 = test positive
-        // inputs; 1 = test negative inputs)
-        static const int zcheckSelPol = 0;
-        // enable Z check
-        static const int zcheckEn = 0;
+        // // RHD2000 Register 5 variables
+        // // impedance testing DAC power-up (0 = power down; 1 = power up)
+        // static const int zcheckDacPower = 0;
+        // // impedance testing dummy load (0 = normal operation; 1 = insert 60 pF to ground)
+        // static const int zcheckLoad = 0;
+        // // impedance testing scale factor:
+        // // 0x00 = 100 fF
+        // // 0x01 = 1.0 pF
+        // // 0x03 = 10.0 pF
+        // static const int zcheckScale = 0;
+        // // impedance testing connect all (0 = normal operation; 1 = connect all electrodes together)
+        // static const int zcheckConnAll = 0;
+        // // impedance testing polarity select (RHD2216 only) (0 = test positive
+        // // inputs; 1 = test negative inputs)
+        // static const int zcheckSelPol = 0;
+        // // enable Z check
+        // static const int zcheckEn = 0;
 
-        // RHD2000 Register 6 variables
-        // this gets written to by aux commands
-        static const int zcheckDac = 128;
+        // // RHD2000 Register 6 variables
+        // // this gets written to by aux commands
+        // static const int zcheckDac = 128;
 
-        // RHD2000 Register 7 variables
-        // impedance testing amplifier select (0-63, but MSB is ignored, so 0-31 in practice)
-        static int zcheckSelect;
+        // // RHD2000 Register 7 variables
+        // // impedance testing amplifier select (0-63, but MSB is ignored, so 0-31 in practice)
+        // static int zcheckSelect;
 
-        // RHD2000 Register 8-13 variables
-        int offChipRH1;         // bandwidth resistor RH1 on/off chip (0 = on chip; 1 = off chip)
-        int offChipRH2;         // bandwidth resistor RH2 on/off chip (0 = on chip; 1 = off chip)
-        int offChipRL;          // bandwidth resistor RL on/off chip (0 = on chip; 1 = off chip)
-        int adcAux1En;          // enable ADC aux1 input (when RH1 is on chip) (0 = disable; 1 = enable)
-        int adcAux2En;          // enable ADC aux2 input (when RH2 is on chip) (0 = disable; 1 = enable)
-        int adcAux3En;          // enable ADC aux3 input (when RL is on chip) (0 = disable; 1 = enable)
+        // // RHD2000 Register 8-13 variables
+        // int offChipRH1;         // bandwidth resistor RH1 on/off chip (0 = on chip; 1 = off chip)
+        // int offChipRH2;         // bandwidth resistor RH2 on/off chip (0 = on chip; 1 = off chip)
+        // int offChipRL;          // bandwidth resistor RL on/off chip (0 = on chip; 1 = off chip)
+        // int adcAux1En;          // enable ADC aux1 input (when RH1 is on chip) (0 = disable; 1 = enable)
+        // int adcAux2En;          // enable ADC aux2 input (when RH2 is on chip) (0 = disable; 1 = enable)
+        // int adcAux3En;          // enable ADC aux3 input (when RL is on chip) (0 = disable; 1 = enable)
 
-        // int rH1Dac1;
-        // int rH1Dac2;
-        // int rH2Dac1;
-        // int rH2Dac2;
-        // int rLDac1;
-        // int rLDac2;
-        // int rLDac3;
+        // // int rH1Dac1;
+        // // int rH1Dac2;
+        // // int rH2Dac1;
+        // // int rH2Dac2;
+        // // int rLDac1;
+        // // int rLDac2;
+        // // int rLDac3;
 
-        // RHD2000 Register 14-17 variables: amplifier power
-        short apwr[4];
+        // // RHD2000 Register 14-17 variables: amplifier power
+        // short apwr[4];
 
-        // double rH1FromUpperBandwidth(double upperBandwidth) const;
-        // double rH2FromUpperBandwidth(double upperBandwidth) const;
-        // double rLFromLowerBandwidth(double lowerBandwidth) const;
-        // double upperBandwidthFromRH1(double rH1) const;
-        // double upperBandwidthFromRH2(double rH2) const;
-        // double lowerBandwidthFromRL(double rL) const;
+        // // double rH1FromUpperBandwidth(double upperBandwidth) const;
+        // // double rH2FromUpperBandwidth(double upperBandwidth) const;
+        // // double rLFromLowerBandwidth(double lowerBandwidth) const;
+        // // double upperBandwidthFromRH1(double rH1) const;
+        // // double upperBandwidthFromRH2(double rH2) const;
+        // // double lowerBandwidthFromRL(double rL) const;
 
 
 };
